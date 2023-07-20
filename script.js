679dd51b57b9a645a396aa3de63b3ed000468538
Shubham Sharma <135977678+shubhamsharmaproch@users.noreply.github.com>
2023-07-20 20:48:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function fibonacci(num) {
  let num1=0; let num2=1; let sum=0;
 	for(let i=0;i<num;i++) {
 		sum=num1+num2;
-		let temp=num1;
-		num1=num2;
-		num2=temp;
+		num1=sum;
+		num2=num1;
+		
 	}
 	return sum;
 }
