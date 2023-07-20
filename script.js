1943cabe35560798d196512990cb747fb30f7a8b
Shubham Sharma <135977678+shubhamsharmaproch@users.noreply.github.com>
2023-07-20 22:51:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function fibonacci(num) {
 	if(num==0) return prev;
 	if(num==1) return curr;
   
-    for (let i = 2; i <= num; i++) {
+    for (let i = 3; i <= num; i++) {
       let next = prev + curr;
       prev = curr;
       curr = next;
