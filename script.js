27b8758f207763d3b95b894dea6b59e29fc19804
Shubham Sharma <135977678+shubhamsharmaproch@users.noreply.github.com>
2023-07-20 20:47:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function fibonacci(num) {
-// your code here
+ let num1=0; let num2=1; let sum=0;
+	for(let i=0;i<num;i++) {
+		sum=num1+num2;
+		let temp=num1;
+		num1=num2;
+		num2=temp;
+	}
+	return sum;
 }
 
 module.exports = fibonacci;
